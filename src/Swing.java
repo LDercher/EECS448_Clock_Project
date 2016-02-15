6fe3cc93b0e0354c39f9ccc6dc29dd00cd84cce8
Victor Berger <victorluiz.m4@gmail.com>
2016-02-14 19:57:14 -0600

    Start working on Javadoc documentation, make minor changes on code (no logical changes)

--- src/Swing.java
+++ src/Swing.java
@@ -1,146 +1,192 @@
-
-//	EECS 448 -- CLOCK PROJECT
-//	JAVA SWING SET - DIGITAL DISPLAY
-//	Original author: VICTOR BERGER - KUID 2770737 - victorluiz@ku.edu
-
-
 import java.awt.*;
+
 import javax.swing.*;
 
+/**
+ * 
+ * @authors Victor Berger, Jesse Yang, Jeromy Tsai, and Cammy Vo
+ * EECS 448 - Clock Project
+ * prof: John Gibbons
+ * code freeze: February 14th, 2016
+ * 
+ */
 
+/**
+ * @param Class
+ *            'Swing' is used to generate and control the Java Swing Set
+ *            functionality of the program.
+ * 
+ */
 public class Swing extends JFrame {
-    WatchPanel watch = new WatchPanel();
-    ConsoleMenu printMenu = new ConsoleMenu();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	WatchPanel watch = new WatchPanel();
+	ConsoleMenu printMenu = new ConsoleMenu();
 
-	public Swing() 
-    {
-        setSize(445, 160);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel pane = new JPanel();
-        pane.setLayout(new GridLayout(1, 1, 15, 15));
-        pane.add(watch);
-        setContentPane(pane);
-        setVisible(true);
-    }
+	/**
+	 * Constructor for the 'Swing' class. Launches clock window, with clock
+	 * originally set at 12:00:00 AM. Will run for entire duration of program.
+	 * 
+	 * @pre -
+	 * @post -
+	 * 
+	 */
+	public Swing() {
+		super("EECS448 - Clock Project");
+		setSize(445, 160);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel pane = new JPanel();
+		pane.setLayout(new GridLayout(1, 1, 15, 15));
+		pane.add(watch);
+		setContentPane(pane);
+		setVisible(true);
+	}
 
-    public static void main(String[] arguments) {
-    	Swing clock = new Swing();
-    }
+	/**
+	 * Main method
+	 * 
+	 */
+	public static void main(String[] arguments) {
+		@SuppressWarnings("unused")
+		Swing clock = new Swing();
+	}
 }
 
-class ConsoleMenu implements Runnable{
-	Thread runner; int test = 0;
+/**
+ * 
+ * Class 'ConsoleMenu' implements the thread used to have the console menu continually running and 
+ * 	able to accept real-time inp
+ *
+ */
+class ConsoleMenu implements Runnable {
+	Thread runner;
+	int test = 0;
+
 	ConsoleMenu() {
-        if (runner == null) {
-            runner = new Thread(this);
-            runner.start();
-        }
-    }
-	public void run(){
+		if (runner == null) {
+			runner = new Thread(this);
+			runner.start();
+		}
+	}
+
+	/**
+	 * Takes clock input from user and makes the necessary changes
+	 * 
+	 * @pre -
+	 * @post -
+	 * @return void
+	 */
+	public void run() {
 		clock a = new clock(1, hhmmss_meridian_military.hh_,
-		hhmmss_meridian_military.mm_,
-		hhmmss_meridian_military.ss_);
-		while(true){
-			
-			//Insert printMenu() call on while loop
-			
+				hhmmss_meridian_military.mm_, hhmmss_meridian_military.ss_);
+		while (true) {
+
 			a.input();
-			
+
 			hhmmss_meridian_military.hh_ = a.hh;
 			hhmmss_meridian_military.mm_ = a.mm;
 			hhmmss_meridian_military.ss_ = a.ss;
-			
-			if(a.AM_or_PM.equals("PM"))
+
+			if (a.AM_or_PM.equals("PM"))
 				hhmmss_meridian_military.AM_or_PM_ = true;
 			else
 				hhmmss_meridian_military.AM_or_PM_ = false;
-			
+
 			hhmmss_meridian_military.military_ = a.militaryTime;
-			  
+
 			hhmmss_meridian_military.timeChanged = true;
-			
+
 			hhmmss_meridian_military.changetimeDifference = a.changetimeDifference;
 		}
 	}
 }
 
-//This class is used to communicate between ConsoleMenu thread and the WatchPanel thread
-//ConsoleMenu takes input from the user to specify hh,mm,ss,AM or PM, or military time
-//Those specified values are set in this class by ContinuosMenu which are then pulled from by WatchPanel.
-//WatchPanel pulls the values then sets them into the clock.
-class hhmmss_meridian_military
-{
+/**
+ * The 'hhmmss_meridian_military' class is used to communicate between ConsoleMenu thread and the
+ * WatchPanel thread ConsoleMenu takes input from the user to specify
+ * hh,mm,ss,AM or PM, or military time Those specified values are set in this
+ * class by ContinuosMenu which are then pulled from by WatchPanel. WatchPanel
+ * pulls the values then sets them into the clock.
+ */
+
+class hhmmss_meridian_military {
 	public static int hh_ = 0;
 	public static int mm_ = 0;
 	public static int ss_ = 0;
-	public static boolean AM_or_PM_ = false;//AM == false, PM == true
+	public static boolean AM_or_PM_ = false;// AM == false, PM == true
 	public static boolean military_ = false;
-	
+
 	public static boolean timeChanged = false;
-	
+
 	public static boolean changetimeDifference = false;
 }
 
 class WatchPanel extends JPanel implements Runnable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	Thread runner;
 
-    WatchPanel() {
-        if (runner == null) {
-            runner = new Thread(this);
-            runner.start();
-        }
-        
-    }
+	WatchPanel() {
+		if (runner == null) {
+			runner = new Thread(this);
+			runner.start();
+		}
 
-    public void run() {
-        while (true) {
-            repaint();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) { }
-        }
-    }
-    
-    clock swingClock = new clock();
-    
-    
-    @Override
-    public void paintComponent(Graphics g) 
-	 {
-    	if(hhmmss_meridian_military.timeChanged)
-    	{
-	    	swingClock.sethh(hhmmss_meridian_military.hh_);
-	    	swingClock.setmm(hhmmss_meridian_military.mm_);
-	    	swingClock.setss(hhmmss_meridian_military.ss_);
-	    	
-	    	if(hhmmss_meridian_military.AM_or_PM_ == true)
-	    		swingClock.AM_or_PM = "PM";
+	}
+
+	public void run() {
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	clock swingClock = new clock();
+
+	/**
+	 * 
+	 * Sets 'paint' configurations of swing window, including parameters such as
+	 * color and font.
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		if (hhmmss_meridian_military.timeChanged) {
+			swingClock.sethh(hhmmss_meridian_military.hh_);
+			swingClock.setmm(hhmmss_meridian_military.mm_);
+			swingClock.setss(hhmmss_meridian_military.ss_);
+
+			if (hhmmss_meridian_military.AM_or_PM_ == true)
+				swingClock.AM_or_PM = "PM";
 			else
 				swingClock.AM_or_PM = "AM";
-	    	
-	    	swingClock.militaryTime = hhmmss_meridian_military.military_;
-	    	
-	    	if(hhmmss_meridian_military.changetimeDifference)
-	    		swingClock.settimeDifference();
-	    	
-	    	hhmmss_meridian_military.timeChanged = false;
-    	}
 
-    	
-    	Font myFont = new Font("Georgia", Font.BOLD, 46);
-        g.setFont(myFont);
-    	if(swingClock.getReadyFlag())
-    	{
- 	        g.setColor(getBackground());
- 	        g.fillRect(0, 0, getSize().width, getSize().height);
-            swingClock.setTime();
-            String time = swingClock.getTime();
-            g.setColor(Color.black);
-            g.drawString(time, 65, 66);
-            	
-    	}
-       
-         
-      }
+			swingClock.militaryTime = hhmmss_meridian_military.military_;
+
+			if (hhmmss_meridian_military.changetimeDifference)
+				swingClock.settimeDifference();
+
+			hhmmss_meridian_military.timeChanged = false;
+		}
+
+		Font myFont = new Font("Georgia", Font.BOLD, 46);
+		g.setFont(myFont);
+		if (swingClock.getReadyFlag()) {
+			g.setColor(getBackground());
+			g.fillRect(0, 0, getSize().width, getSize().height);
+			swingClock.setTime();
+			String time = swingClock.getTime();
+			g.setColor(Color.black);
+			g.drawString(time, 65, 66);
+
+		}
+
+	}
 }
--- src/clock.java
+++ src/clock.java
@@ -7,329 +7,300 @@ import org.joda.time.format.DateTimeFormatter;
 
 /**
  * 
- * @author Jesse Yang
- * @author Cammy Vo 
- * @author Jeromy Tsai 
- * @author Victor Luiz
- * @see http://stackoverflow.com/questions/12105164/java-joda-time-download-and-install-step-by-step
- *
+ * @authors Victor Berger, Jesse Yang, Jeromy Tsai, and Cammy Vo
+ * EECS 448 - Clock Project
+ * prof: John Gibbons
+ * code freeze: February 14th, 2016
+ * 
+ * @see http://stackoverflow.com/questions/12105164/java-joda-time-download-and-install-step-by-ste
  */
+
 
 /**
- * How to import timing library taken from: http://stackoverflow.com/questions/12105164/java-joda-time-download-and-install-step-by-step
- * Clock class
+ * How to import timing library taken from:
+ * http://stackoverflow.com/questions/12105164
+ * /java-joda-time-download-and-install-step-by-step Clock class
  */
-public class clock
-{
- /** constructor method*/
-//constructor method
-	 public clock()
-	 {
-	  hh = 0; 
-	  mm = 0; 
-	  ss = 0; 
-	  AM_or_PM = "AM";
-	  timeString = "";
-	  timeDifference = 0;
-	  //input();
-	  
-	  
-	  try {
-	   initializeClock();
-	  } catch (InterruptedException e) {
-	   // TODO Auto-generated catch block
-	   e.printStackTrace();
-	  } catch (IOException e) {
-	   // TODO Auto-generated catch block
-	   e.printStackTrace();
-	  }
-	
-	 }
-	 public clock(int menu, int hh, int mm, int ss)
-	 {
-		 //pass an integer to skip input() call; this is used to get menu output
-		 this.hh = hh; 
-		  this.mm = mm; 
-		  this.ss = ss; 
-		  AM_or_PM = "AM";
-		  timeString = "";
-		  timeDifference = 0;
-	 }
-	
+public class clock {
+	/** constructor method */
 
- /**
-  * hh is hours, mm is minutes, ss is seconds, AM_or_PM is AM or PM, militaryTime switches 12-24 hour format
-  */
- int hh;
- int mm;
- int ss;
- String AM_or_PM;
- boolean militaryTime = false;
- String Num_S = "";
- String timeString;
- boolean readyFlag = false;
- int timeDifference;
- boolean changetimeDifference = false;
- 
- /** @return timeString*/
- public String getTime()
- {
-	 return timeString;
- }
- 
- /** set timeString variable*/
- public void setTime()
- {
-	 DateTimeFormatter dtf = DateTimeFormat.forPattern("mm/dd/yyyy hh:mm:ss a");
-	 /** Parsing the date*/
-	 DateTime jodatime = dtf.parseDateTime(dtf.print(DateTime.now().plusMillis(timeDifference)));
-	 /** Format for output*/
-	 DateTimeFormatter dtfOut;
-	 if(militaryTime == true)
-	 {
-		 dtfOut = DateTimeFormat.forPattern("HH:mm:ss");
-	 }
-	 else
-	 {
-		 dtfOut = DateTimeFormat.forPattern("hh:mm:ss a");
-	 }
-	 /** Printing the time*/
-	 timeString = dtfOut.print(jodatime);
-	 /** old code*/
-	 //System.out.println(dtfOut.print(jodatime));
-	 //Thread.sleep(700); //This line is put in to prevent the terminal from getting spammed.
- }
-	
-	 public void sethh(int hh)
-	 {
-		 this.hh = hh;
-	 }
-	 
-	 public int gethh()
-	 {
-		 return hh;
-	 }
-	 
-	 public void setmm(int mm)
-	 {
-		 this.mm = mm;
-	 }
-	 
-	 public int getmm()
-	 {
-		 return mm;
-	 }
-	 
-	 public void setss(int ss)
-	 {
-		 this.ss = ss;
-	 }
-	 
-	 public int getss()
-	 {
-		 return ss;
-	 }
- 
-	 public void settimeDifference()
-	 {
-		// Convert hh to 24 hour equiv; used to calculate the milliseconds of the day
-		  int HH = hh;
-			  if(AM_or_PM.equals("PM") && HH != 12)//if PM add 12 hours except if it is 12 PM
-			  { HH += 12; }
-			   else if(AM_or_PM.equals("AM") && HH == 12)//if 12 AM, it is 0 hours from midnight
-			   { HH = 0; }
-		  
-		 timeDifference = (HH * 3600 + mm * 60 + ss) * 1000 - DateTime.now().getMillisOfDay();
-	 }
- 
- public boolean getReadyFlag()
- {
-	 return readyFlag;
- }
- /**
- //This method will take the user specified input for hh,mm,ss,AMorPM.
- //I assume this method is only temporary and will be changed as we employ a GUI.
- //Note there is no exception handling for this method yet so giving non-numerical input for hh,mm, or ss crashes the program.
- */
- public void input() 
- {
-  // TODO Auto-generated method stub
-  Scanner input = new Scanner(System.in);
-  
-  int temp = -1;
+	public clock() {
+		hh = 0;
+		mm = 0;
+		ss = 0;
+		AM_or_PM = "AM";
+		timeString = "";
+		timeDifference = 0;
 
-  System.out.println("Choose:");
-  System.out.println("1.) military time(24 hour)");
-  System.out.println("2.) Non-military time(12 hour): ");
-  System.out.println("3.) Switch between AM/PM and military time formats");
-  while(temp < 1 || temp > 3)
-  {
-	    Num_S = input.nextLine();
-	   while (!Num_S.matches("\\d+$")) //from: http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
-	  {
-		 System.out.print("not valid number. try again: ");
-		 Num_S = input.nextLine();
-	  }
-	   temp = Integer.parseInt(Num_S);
-	  //temp = input.nextInt();
-	  if(temp < 1 || temp > 3)
-	  {
-		  System.out.println("Choose:");
-		  System.out.println("1.) military time(24 hour)");
-		  System.out.println("2.) Non-military time(12 hour): ");
-		  System.out.println("3.) Switch between AM/PM and military time formats");
-	  }
-	  if(temp == 1)
-	  {
-		  militaryTime = true;
-	  }
-	  if(temp == 2)
-	  {
-		  militaryTime = false;
-	  }
-	  if(temp == 3)
-	  {  militaryTime = !militaryTime; changetimeDifference = false;}
-  }
-  /** set hh*/
-  
-  if(temp == 1 || temp == 2)
-  {
-	  hh = -1; 
-	  mm = -1; 
-	  ss = -1;
-  System.out.print("Insert hh:");
-  if(militaryTime == true)
-  {
-	  while(hh < 0 || hh > 23)
-	  {
-		    Num_S = input.nextLine();
-			   while (!Num_S.matches("\\d+$")) //from: http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
-			  {
-				 System.out.print("not valid number. try again: ");
-				 Num_S = input.nextLine();
-			  }
-			   hh = Integer.parseInt(Num_S);
-	   //hh = input.nextInt();
-	   if(hh > 12)
-	   {   AM_or_PM = "PM";	hh -=12;}
-	   else
-	   {
-		   AM_or_PM = "AM";
-		   if(hh == 0)
-			   hh = 12;
-	   }
-	   
-	   if(hh < 0 || hh > 23)
-	   {
-	    System.out.print("hh must be between 0-23, try again:");
-	   }
-	  }
-  }
-  else
-  {
-	  while(hh < 1 || hh > 12)
-	  {
-		    Num_S = input.nextLine();
-			   while (!Num_S.matches("\\d+$")) //from: http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
-			  {
-				 System.out.print("not valid number. try again: ");
-				 Num_S = input.nextLine();
-			  }
-			   hh = Integer.parseInt(Num_S);
-		   //hh = input.nextInt();
-		   if(hh < 1 || hh > 12)
-		   {
-			   System.out.print("hh must be between 1-12, try again:");
-		   }
-	  }
-  }
-  
-  /** set mm*/
-  System.out.print("Insert mm:");
-  while(mm < 0 || mm > 59)
-  {
-	    Num_S = input.nextLine();
-	   while (!Num_S.matches("\\d+$")) //from: http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
-	  {
-		 System.out.print("not valid number. try again: ");
-		 Num_S = input.nextLine();
-	  }
-	   mm = Integer.parseInt(Num_S);
-  // mm = input.nextInt();
-   if(mm < 0 || mm > 59)
-   {
-    System.out.print("mm must be between 0-59, try again:");
-   }
-  }
-  /** set ss*/
-  System.out.print("Insert ss:");
-  while(ss < 0 || ss > 59)
-  {
-	    Num_S = input.nextLine();
-	   while (!Num_S.matches("\\d+$")) //from: http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
-	  {
-		 System.out.print("not valid number. try again: ");
-		 Num_S = input.nextLine();
-	  }
-	   ss = Integer.parseInt(Num_S);
-   //ss = input.nextInt();
-   if(ss < 0 || ss > 59)
-   {
-    System.out.print("ss must be between 0-59, try again:");
-   }
-  }
-  if (militaryTime == false)
-  {
-	  System.out.print("Insert AM or PM:");
-	  AM_or_PM = input.nextLine();
-	  while(!AM_or_PM.equalsIgnoreCase("AM") && !AM_or_PM.equalsIgnoreCase("PM"))
-	  {
-		  AM_or_PM = input.nextLine();
-		  if(!AM_or_PM.equalsIgnoreCase("AM") && !AM_or_PM.equalsIgnoreCase("PM"))
-		  {
-			  System.out.print("AM or PM, try again:");
-		  }
-	  }
-  }
-  changetimeDifference = true;
-  }
- }
- /** set AM or PM*/
-  public void AMorPM(){
-  Scanner input = new Scanner(System.in);
-  System.out.print("Insert AM or PM:");
-  AM_or_PM = input.nextLine();
-  while(!AM_or_PM.equals("AM") && !AM_or_PM.equals("PM"))
-  {
-   AM_or_PM = input.nextLine();
-   if(!AM_or_PM.equals("AM") && !AM_or_PM.equals("PM"))
-   {
-    System.out.print("AM or PM, try again:");
-   }
-  }
-  input.close();
-  
- }
- 
+		try {
+			initializeClock();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
- //This is the important part of the class. This will print the Clock in hh:mm:ss XX format. 
- //It will also utilize the joda-time library. 
-  /**
-   * HOW TIME IS CALCULATED: timeDifference between actual time and user specified time is calculated.
-   * This difference is used to find the user specified time on each iteration. 
-   * @see http://kodejava.org/how-to-add-hours-minutes-seconds-into-datetime-in-joda-time/
-   * @see http://stackoverflow.com/questions/20331163/how-to-format-joda-time-datetime-to-only-mm-dd-yyyy
-   * @throws InterruptedException
-   * @throws IOException
-   */
-private void initializeClock() throws InterruptedException, IOException 
-	 { 
-	  settimeDifference();
-	
-	  System.out.println("---------------print clock---------------");
-	  
-	  readyFlag = true;
-	  
-	 }
+	}
+
+	public clock(int menu, int hh, int mm, int ss) {
+		// pass an integer to skip input() call; this is used to get menu output
+		this.hh = hh;
+		this.mm = mm;
+		this.ss = ss;
+		AM_or_PM = "AM";
+		timeString = "";
+		timeDifference = 0;
+	}
+
+	/**
+	 * hh is hours, mm is minutes, ss is seconds, AM_or_PM is AM or PM,
+	 * militaryTime switches 12-24 hour format
+	 */
+	int hh;
+	int mm;
+	int ss;
+	String AM_or_PM;
+	boolean militaryTime = false;
+	String Num_S = "";
+	String timeString;
+	boolean readyFlag = false;
+	int timeDifference;
+	boolean changetimeDifference = false;
+
+	/** @return timeString */
+	public String getTime() {
+		return timeString;
+	}
+
+	/** set timeString variable */
+	public void setTime() {
+		DateTimeFormatter dtf = DateTimeFormat
+				.forPattern("mm/dd/yyyy hh:mm:ss a");
+		/** Parsing the date */
+		DateTime jodatime = dtf.parseDateTime(dtf.print(DateTime.now()
+				.plusMillis(timeDifference)));
+		/** Format for output */
+		DateTimeFormatter dtfOut;
+		if (militaryTime == true) {
+			dtfOut = DateTimeFormat.forPattern("HH:mm:ss");
+		} else {
+			dtfOut = DateTimeFormat.forPattern("hh:mm:ss a");
+		}
+		/** Printing the time */
+		timeString = dtfOut.print(jodatime);
+
+	}
+
+	public void sethh(int hh) {
+		this.hh = hh;
+	}
+
+	public int gethh() {
+		return hh;
+	}
+
+	public void setmm(int mm) {
+		this.mm = mm;
+	}
+
+	public int getmm() {
+		return mm;
+	}
+
+	public void setss(int ss) {
+		this.ss = ss;
+	}
+
+	public int getss() {
+		return ss;
+	}
+
+	public void settimeDifference() {
+		// Convert hh to 24 hour equivalent; used to calculate the milliseconds of
+		// the day
+		int HH = hh;
+		if (AM_or_PM.equals("PM") && HH != 12)// if PM add 12 hours except if it
+												// is 12 PM
+		{
+			HH += 12;
+		} else if (AM_or_PM.equals("AM") && HH == 12)// if 12 AM, it is 0 hours
+														// from midnight
+		{
+			HH = 0;
+		}
+
+		timeDifference = (HH * 3600 + mm * 60 + ss) * 1000
+				- DateTime.now().getMillisOfDay();
+	}
+
+	public boolean getReadyFlag() {
+		return readyFlag;
+	}
+
+	public void input() {
+		Scanner input = new Scanner(System.in);
+
+		int temp = -1;
+
+		System.out.println("Choose:");
+		System.out.println("1.) Military time(24 hour)");
+		System.out.println("2.) Non-military time(12 hour) ");
+		System.out
+				.println("3.) Switch between AM/PM and military time formats");
+		while (temp < 1 || temp > 3) {
+			Num_S = input.nextLine();
+			while (!Num_S.matches("\\d+$")) // from:
+											// http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
+			{
+				System.out.print("not valid number. try again: ");
+				Num_S = input.nextLine();
+			}
+			temp = Integer.parseInt(Num_S);
+			if (temp < 1 || temp > 3) {
+				System.out.println("Choose:");
+				System.out.println("1.) military time(24 hour)");
+				System.out.println("2.) Non-military time(12 hour): ");
+				System.out.println("3.) Switch between AM/PM and time formats");
+			}
+			if (temp == 1) {
+				militaryTime = true;
+			}
+			if (temp == 2) {
+				militaryTime = false;
+			}
+			if (temp == 3) {
+				militaryTime = !militaryTime;
+				changetimeDifference = false;
+			}
+		}
+		/** set hh */
+
+		if (temp == 1 || temp == 2) {
+			hh = -1;
+			mm = -1;
+			ss = -1;
+			System.out.print("Insert hh:");
+			if (militaryTime == true) {
+				while (hh < 0 || hh > 23) {
+					Num_S = input.nextLine();
+					while (!Num_S.matches("\\d+$")) // from:
+													// http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
+					{
+						System.out.print("Invalid number. Please try again: ");
+						Num_S = input.nextLine();
+					}
+					hh = Integer.parseInt(Num_S);
+					if (hh > 12) {
+						AM_or_PM = "PM";
+						hh -= 12;
+					} else {
+						AM_or_PM = "AM";
+						if (hh == 0)
+							hh = 12;
+					}
+
+					if (hh < 0 || hh > 23) {
+						System.out.print("HH must be between 0-23, try again:");
+					}
+				}
+			} else {
+				while (hh < 1 || hh > 12) {
+					Num_S = input.nextLine();
+					while (!Num_S.matches("\\d+$")) // from:
+													// http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
+					{
+						System.out.print("Invalid number. Please try again: ");
+						Num_S = input.nextLine();
+					}
+					hh = Integer.parseInt(Num_S);
+					if (hh < 1 || hh > 12) {
+						System.out.print("HH must be between 1-12, try again:");
+					}
+				}
+			}
+
+			/** set mm */
+			System.out.print("Insert mm:");
+			while (mm < 0 || mm > 59) {
+				Num_S = input.nextLine();
+				while (!Num_S.matches("\\d+$")) // from:
+												// http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
+				{
+					System.out.print("Invalid number. Please try again: ");
+					Num_S = input.nextLine();
+				}
+				mm = Integer.parseInt(Num_S);
+				if (mm < 0 || mm > 59) {
+					System.out.print("MM must be between 0-59, try again:");
+				}
+			}
+			/** set ss */
+			System.out.print("Insert ss:");
+			while (ss < 0 || ss > 59) {
+				Num_S = input.nextLine();
+				while (!Num_S.matches("\\d+$")) // from:
+												// http://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java
+				{
+					System.out.print("Invalid number. Please try again: ");
+					Num_S = input.nextLine();
+				}
+				ss = Integer.parseInt(Num_S);
+				if (ss < 0 || ss > 59) {
+					System.out.print("SS must be between 0-59, try again:");
+				}
+			}
+			if (militaryTime == false) {
+				System.out.print("Insert AM or PM:");
+				AM_or_PM = input.nextLine();
+				while (!AM_or_PM.equalsIgnoreCase("AM")
+						&& !AM_or_PM.equalsIgnoreCase("PM")) {
+					AM_or_PM = input.nextLine();
+					if (!AM_or_PM.equalsIgnoreCase("AM")
+							&& !AM_or_PM.equalsIgnoreCase("PM")) {
+						System.out.print("AM or PM, try again:");
+					}
+				}
+			}
+			changetimeDifference = true;
+		}
+	}
+
+	/** set AM or PM */
+	public void AMorPM() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Insert AM or PM:");
+		AM_or_PM = input.nextLine();
+		while (!AM_or_PM.equals("AM") && !AM_or_PM.equals("PM")) {
+			AM_or_PM = input.nextLine();
+			if (!AM_or_PM.equals("AM") && !AM_or_PM.equals("PM")) {
+				System.out.print("AM or PM, try again:");
+			}
+		}
+		input.close();
+
+	}
+
+	/**
+	 * This is the important part of the class. This will print the Clock in
+	 * hh:mm:ss XX format. It will also utilize the joda-time library.
+	 *
+	 * HOW TIME IS CALCULATED: timeDifference between actual time and user
+	 * specified time is calculated. This difference is used to find the user
+	 * specified time on each iteration.
+	 * 
+	 * @see http 
+	 *      ://kodejava.org/how-to-add-hours-minutes-seconds-into-datetime-in
+	 *      -joda-time/
+	 * @see http 
+	 *      ://stackoverflow.com/questions/20331163/how-to-format-joda-time-
+	 *      datetime -to-only-mm-dd-yyyy
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	private void initializeClock() throws InterruptedException, IOException {
+		settimeDifference();
+		readyFlag = true;
+
+	}
 
 }
